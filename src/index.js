9885b90ade30c49d8554c9cf173a0389fa347167
Paulo de Tasso <xcloudgameemail@gmail.com>
2022-08-28 18:35:41 -0300

    feat: implements edit todos by id

--- src/index.js
+++ src/index.js
@@ -75,7 +75,35 @@ app.post('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  if (!title || !deadline) {
+    return response.status(400).json({ error: 'Title or deadline is missing' });
+  }
+
+  if (!id) {
+    return response.status(400).json({ error: 'Id is missing' });
+  }
+
+  const userFiltered = users.find(user => user.username === username);
+
+  const todoFiltered = userFiltered.todos.find(todo => todo.id === id);
+
+  if (!todoFiltered) {
+    return response.status(404).json({ error: 'Todo not found' });
+  }
+
+  const todoUpdated = {
+    ...todoFiltered,
+    title: title,
+    deadline: new Date(deadline)
+  }
+
+  userFiltered.todos.splice(todoFiltered, 1, todoUpdated);
+
+  return response.status(200).json(todoUpdated);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
